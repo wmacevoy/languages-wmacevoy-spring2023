ec3b19408471566f4d44fb00141c5e01de3c10fc
Warren MacEvoy <wmacevoy@gmail.com>
2023-02-06 12:38:41 -0700

    test proc vs func

---
+++ cpp/procedural-vs-functional/include/sorts.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> sort_func(std::vector<std::string> items);
+void sort_proc(std::vector<std::string> &items);
+
+
---
+++ cpp/procedural-vs-functional/src/sorts.cpp
@@ -0,0 +1,10 @@
+#include "sorts.h"
+
+std::vector<std::string> sort_func(std::vector<std::string> items) {
+   // TODO sort the items using a functional programming style.
+   return std::vector < std::string > ();
+}
+
+void sort_proc(std::vector<std::string> &items) {
+  // TODO sort the items using a procedural programming style.
+}
